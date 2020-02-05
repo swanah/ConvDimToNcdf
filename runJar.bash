48c63bb2a94e35ee87757c54879c3446a251fbbc
swanah <akheckel@uni-bremen.de>
2020-02-05 17:41:15 +0100

    file cleanup

--- runJar.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-THISPATH=/geog/local/alpha/progs/java/convDimToNcdf/
-CPSEP=$(uname -o|awk '/Linux/{printf(":")} /Cygwin/{printf(";")}')
-CLASSPATH=${CLASSPATH}${CPSEP}$(find ${BEAM4_HOME} -name \*.jar -printf "%p${CPSEP}")
-CLASSPATH=${CLASSPATH}$(find $THISPATH -name \*.jar -printf "%p${CPSEP}")
-export CLASSPATH
-#echo $CLASSPATH
-
-java akh.convdimtoncdf.ConvDimToNcdf $*
